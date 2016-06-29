9da583e52b85ec611d480c394df022be8857a3eb
Alex Y. Chan <ayb.chan@gmail.com>
2016-06-29 20:57:41 +0100

    Add function to read saved data

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import os
-import json
 import time
+import json
+import fnmatch
 
 def save(data):
   if not os.path.exists('data'):
@@ -9,3 +10,13 @@ def save(data):
   filename = 'data/' + time.strftime("%Y%m%d-%H%M%S") + '.json'
   with open(filename, 'w') as f:
     json.dump(data,f)
+
+def read():
+  d = []
+  for file in os.listdir('data'):
+    if fnmatch.fnmatch(file, '*.json'):
+      with open('data/' + file) as f:
+        data = json.load(f)
+      d.append(data)
+  return d
+
