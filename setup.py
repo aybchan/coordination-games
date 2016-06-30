7674303f118bc0550eae0f37e4e407f220236155
Alex Y. Chan <ayb.chan@gmail.com>
2016-06-30 17:55:25 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Coordination-Games',
+    version="1.0.0",
+    packages=[''],
+    author='Alex Y. Chan',
+    requires=['numpy','json','matplotlib','tabulate']
+    )
+
