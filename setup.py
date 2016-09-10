bbf98f2ed9eac3de03dacc270450c7272f6e76d0
Alex Y. Chan <ayb.chan@gmail.com>
2016-09-10 18:04:56 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='Coordination-Games',
     version="2.0.0",
     author='Alex Y. Chan',
-    author_email='ayb.chan@gmail.com'
+    author_email='ayb.chan@gmail.com',
     license=license,
     packages=find_packages()
 )
